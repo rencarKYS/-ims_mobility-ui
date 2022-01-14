aace765722ab509a7715f57674866296cd99d14b
kangyongsuek <kangyongsuek@rencar.co.kr>
2022-01-14 13:02:49 +0900

    modify: 수정

--- components/index.js
+++ components/index.js
@@ -1,2 +1,6 @@
-export { default as ImsInput } from './Input'
-export { default as ImsButton } from './Button'
+import ImsInput from './Input'
+import ImsButton from './Button'
+
+export { ImsInput, ImsButton }
+// export { default as ImsInput } from './Input'
+// export { default as ImsButton } from './Button'
